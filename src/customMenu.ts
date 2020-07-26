f921bec5de37df42e3b74cccb1516df38100f34b
Noah Hamilton <hamiltonn@usf.edu>
2020-07-26 19:56:09 -0400

    Add a custom file

---
+++ src/customMenu.ts
@@ -0,0 +1,39 @@
+/** Accesses UI and creates menu items for the addon.s */
+export function createMenu(): void {
+  const ui = SpreadsheetApp.getUi();
+  // Or DocumentApp or FormApp.
+  ui.createMenu("Custom Menu")
+    .addItem("Download Rows to PDF Purchase Form", "downloadSheet")
+    .addSeparator()
+    .addItem("Read Row", "readRows")
+    .addSeparator()
+    .addItem("Send Email to Treasurer", "sendEmail")
+    .addToUi();
+}
+
+export function downloadSheet(): void {
+  SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
+    .alert("You clicked the download pdf item!");
+}
+
+export function sendEmail(): void {
+  SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
+    .alert("You clicked the send email item!");
+}
+
+export function readRows(): void {
+  const sheet = SpreadsheetApp.getActiveSheet();
+  const data = sheet.getDataRange().getValues();
+  for (let i = 0; i < data.length; i++) {
+    Logger.log("Item name: " + data[i][0]);
+    Logger.log("Vendor number: " + data[i][1]);
+    Logger.log("Vendor URL: " + data[i][2]);
+    Logger.log("Cost: " + data[i][3]);
+
+    Logger.log("Quantity: " + data[i][4]);
+    Logger.log("Item URL: " + data[i][5]);
+    Logger.log("Project: " + data[i][6]);
+    Logger.log("Date Needed: " + data[i][7]);
+  }
+  SpreadsheetApp.getUi().alert(Logger.getLog());
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // You can access any of the global GAS objects in this file. You can also
 // import local files or external dependencies:
 import { helloWorld } from "./example";
+import * as CM from "./customMenu";
 
 console.log(helloWorld);
 
@@ -10,33 +11,6 @@ console.log(helloWorld);
 // for anything else.
 // See: https://developers.google.com/apps-script/guides/triggers
 
-function menuItem1(): void {
-  SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
-    .alert("You clicked the download pdf item!");
-}
-
-function menuItem2(): void {
-  SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
-    .alert("You clicked the send email item!");
-}
-
-function readRows(): void {
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const data = sheet.getDataRange().getValues();
-  for (let i = 0; i < data.length; i++) {
-    Logger.log("Item name: " + data[i][0]);
-    Logger.log("Vendor number: " + data[i][1]);
-    Logger.log("Vendor URL: " + data[i][2]);
-    Logger.log("Cost: " + data[i][3]);
-
-    Logger.log("Quantity: " + data[i][4]);
-    Logger.log("Item URL: " + data[i][5]);
-    Logger.log("Project: " + data[i][6]);
-    Logger.log("Date Needed: " + data[i][7]);
-  }
-  SpreadsheetApp.getUi().alert(Logger.getLog());
-}
-
 export function onOpen(
   e:
     | GoogleAppsScript.Events.DocsOnOpen
@@ -45,14 +19,7 @@ export function onOpen(
     | GoogleAppsScript.Events.FormsOnOpen
 ): void {
   console.log(e);
-  const ui = SpreadsheetApp.getUi();
-  // Or DocumentApp or FormApp.
-  ui.createMenu("Custom Menu")
-    .addItem("Download Rows to PDF Purchase Form", "menuItem1")
-    .addItem("Read Row", "readRows")
-    .addSeparator()
-    .addItem("Send Email to Treasurer", "menuItem2")
-    .addToUi();
+  CM.createMenu();
 }
 
 export function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit): void {
@@ -77,6 +44,6 @@ global.onInstall = onInstall;
 global.doPost = doPost;
 global.doGet = doGet;
 // Be sure to define all custom functions as global variables
-global.menuItem1 = menuItem1;
-global.menuItem2 = menuItem2;
-global.readRows = readRows;
+global.downloadSheet = CM.downloadSheet;
+global.sendEmail = CM.sendEmail;
+global.readRows = CM.readRows;
