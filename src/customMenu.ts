1ad41d30b9a373340091f86bb2ca0a141e592c5a
Noah Hamilton <hamiltonn@usf.edu>
2020-07-26 20:20:13 -0400

    Select only active range.

--- src/customMenu.ts
+++ src/customMenu.ts
@@ -12,6 +12,10 @@ export function createMenu(): void {
 }
 
 export function downloadSheet(): void {
+  // read the selected rows
+  // duplicate the Template PO sheet
+  // Write the row info to the duplicated sheet.
+
   SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
     .alert("You clicked the download pdf item!");
 }
@@ -23,17 +27,27 @@ export function sendEmail(): void {
 
 export function readRows(): void {
   const sheet = SpreadsheetApp.getActiveSheet();
-  const data = sheet.getDataRange().getValues();
-  for (let i = 0; i < data.length; i++) {
-    Logger.log("Item name: " + data[i][0]);
-    Logger.log("Vendor number: " + data[i][1]);
-    Logger.log("Vendor URL: " + data[i][2]);
-    Logger.log("Cost: " + data[i][3]);
+  const range = sheet.getActiveRange();
+  if (range == null) {
+    SpreadsheetApp.getUi().alert(
+      "Please select a range to read from. You can select multiple rows by pressing shift and clicking."
+    );
+  } else {
+    const data = range.getValues();
 
-    Logger.log("Quantity: " + data[i][4]);
-    Logger.log("Item URL: " + data[i][5]);
-    Logger.log("Project: " + data[i][6]);
-    Logger.log("Date Needed: " + data[i][7]);
+    for (let i = 0; i < data.length; i++) {
+      Logger.log("Item name: " + data[i][0]);
+      Logger.log("Vendor number: " + data[i][1]);
+      Logger.log("Vendor URL: " + data[i][2]);
+      Logger.log("Cost: " + data[i][3]);
+
+      Logger.log("Quantity: " + data[i][4]);
+      Logger.log("Item URL: " + data[i][5]);
+      Logger.log("Project: " + data[i][6]);
+      Logger.log("Date Needed: " + data[i][7]);
+    }
+    SpreadsheetApp.getUi().alert(Logger.getLog());
   }
-  SpreadsheetApp.getUi().alert(Logger.getLog());
 }
+
+// TODO: Add data validation
